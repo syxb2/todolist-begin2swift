2b2dca688553e0e2a2061a8a51b3197358b3527e
syxb2 <578767478@qq.com>
2025-09-22 17:20:13 +0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by 白家乐 on 2025/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by 白家乐 on 2025/9/22.
+//
+
+import Testing
+@testable import ToDoList
+
+struct ToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
